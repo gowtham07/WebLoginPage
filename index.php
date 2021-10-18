2f83506a03fa143f7fe09fbd699299242e9134b6
GowthamKrishna <gowthamaddluri@gmail.com>
2021-10-18 23:41:16 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -17,7 +17,15 @@ require_once "pdo.php";
 </p>
 <p>
 Attempt to go to 
-<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+
+</p>
+
+<p>
+
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+
+</p>
 
 <p><strong>Note:</strong>  Your implementation should retain data across multiple logout/login sessions. This sample implementation clears all its data on logout - which you should not do in your implementation.
 </p>
--- login.php
+++ login.php
@@ -1,6 +1,41 @@
 <?php // Do not put any HTML above this line
+session_start();
 
-require_once "pdo.php";
+
+// if ( isset($_POST['guess']) ) {
+// $guess = $_POST['guess'] + 0;
+// $_SESSION['guess'] = $guess;
+// if ( $guess == 42 ) {
+// $_SESSION['message'] = "Great job!";
+// } else if ( $guess < 42 ) {
+// $_SESSION['message'] = "Too low";
+// } else {
+// $_SESSION['message'] = "Too high...";
+// }
+// header("Location: guess2.php");
+// return;
+// }
+
+
+
+
+
+// if ( isset($_POST["who"]) && isset($_POST["pass"]) ) {
+//                    // Logout current user
+//         if ( $_POST['pass'] == 'php123' ) 
+//         {
+//             $_SESSION["who"] = $_POST["who"];
+//             $_SESSION["who"]
+           
+//         } 
+           
+//        header( 'Location: login.php' ) ;
+//             return;     
+        
+//     }
+
+
+// require_once "pdo.php";
 if ( isset($_POST['cancel'] ) ) {
     // Redirect the browser to autos.php
     header("Location: index.php");
@@ -15,14 +50,21 @@ $failure = false;  // If we have no POST data
 // Check to see if we have some POST data, if we do process it
 if ( isset($_POST['who']) && isset($_POST['pass']) )
  {
+       unset($_SESSION["who"]);
+       unset($_SESSION["error"]);
+       unset($_SESSION["success"]);
+       unset($_SESSION["pass"]);
+
     if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) 
     {
         $failure = "User name and password are required";
+        $_SESSION["error"] = $failure;
     } 
 
     elseif (!(strpos($_POST['who'],'@') !== false))
     {
         $failure = "@ is missing";
+         $_SESSION["error"] = $failure;
     }
 
     else 
@@ -31,7 +73,9 @@ if ( isset($_POST['who']) && isset($_POST['pass']) )
         if ( $check == $stored_hash ) 
         {
             // Redirect the browser to game.php
-            header("Location: autos.php?name=".urlencode($_POST['who']));
+            $_SESSION["who"] = $_POST["who"];
+            $_SESSION["success"] = "Logged in.";
+            header("Location: view.php");
             error_log("Login success ".$_POST['who']);
             return;
         } 
@@ -41,16 +85,45 @@ if ( isset($_POST['who']) && isset($_POST['pass']) )
             
 
             error_log("Login fail ".$_POST['who']." $check");
+            $_SESSION["error"] = "Incorrect password.";
+            header( 'Location: login.php' ) ;
+            return;
         }
     }
+
+    if($_SESSION["error"] == "Incorrect password." || $_SESSION["error"] == "User name and password are required"|| $_SESSION["error"] == "@ is missing")
+    {
+          header( 'Location: login.php' ) ;
+          return;
+    }
+
+    if($_SESSION["success"] == "Logged in.")
+    {
+        header("Location: view.php");
+    }
+
 }
+
+// else
+// {
+
+//     if($_SESSION["error"] == "Incorrect password." || $_SESSION["error"] == "User name and password are required"|| $_SESSION["error"] == "@ is missing")
+//     {
+//           header( 'Location: login.php' ) ;
+//     }
+
+//     if($_SESSION["success"] == "Logged in.")
+//     {
+//         header("Location: view.php");
+//     }
+// }
+
 
 // Fall through into the View
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<?php require_once "bootstrap.php"; ?>
 <title>gowtham</title>
 </head>
 <body>
@@ -59,10 +132,13 @@ if ( isset($_POST['who']) && isset($_POST['pass']) )
 <?php
 // Note triple not equals and think how badly double
 // not equals would work here...
-if ( $failure !== false ) {
-    // Look closely at the use of single and double quotes
-    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
-}
+
+    if ( isset($_SESSION["error"]) ) {
+        echo('<p style="color:red">'.$_SESSION["error"]."</p>\n");
+        unset($_SESSION["error"]);
+    }
+
+    
 ?>
 <form method="POST">
 <label for="nam">User Name</label>
